bcfcbe84c2483d141f7dfafe67d4b2b81b8b24b7
Melisa Mustafa <melisa.mustafa@auroralabs.com>
2026-01-29 16:54:12 +0100

    PR

--- blinky/Blinky_LED.c
+++ blinky/Blinky_LED.c
@@ -50,9 +50,26 @@ void initLED(void)
     IfxPort_setPinHigh(LED_D107);
 }
 
+__attribute__((noinline)) static void simulateCpuWorkload(void)
+{
+    /*
+     * Intentional extra CPU work to simulate a measurable runtime slowdown.
+     * This kind of thing sometimes appears during quick bring-up/debug when
+     * someone experiments with timing and forgets to remove it.
+     */
+    volatile uint32 checksum = 0u;
+    for (uint32 i = 0u; i < 500000u; ++i)
+    {
+        checksum += (i ^ (checksum >> 3)) + 0x9e3779b9u;
+    }
+    (void)checksum;
+}
+
+
 /* This function toggles the port pin and wait 500 milliseconds */
 void blinkLED(void)
 {
     IfxPort_togglePin(LED_D107);                                                /* Toggle the state of the LED      */
+    simulateCpuWorkload();
     waitTime(IfxStm_getTicksFromMilliseconds(BSP_DEFAULT_TIMER, WAIT_TIME));    /* Wait 500 milliseconds            */
 }
