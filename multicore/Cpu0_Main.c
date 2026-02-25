700a8b45f5089b8acb82e6a77eeaee9e56e5a455
Oneirarch <nazorien@gmail.com>
2026-02-25 21:24:50 +0200

    test(PERF-003s): add light FNV-1a hash workload for small degradation

--- multicore/Cpu0_Main.c
+++ multicore/Cpu0_Main.c
@@ -41,6 +41,7 @@
 #include "IfxCpu.h"
 #include "IfxScuWdt.h"
 #include "Multicore.h"
+#include "perf_light.h"
 
 IFX_ALIGN(4) IfxCpu_syncEvent g_cpuSyncEvent = 0;
 
@@ -67,6 +68,6 @@ void core0_main(void)
 
     while (1)
     {
-        turnLEDon(); /* If the global variable g_turnLEDon is TRUE, turn on the LED */
+        { volatile uint32 r = light_hash(0xDEADu, 100); (void)r; } turnLEDon(); /* If the global variable g_turnLEDon is TRUE, turn on the LED */
     }
 }
---
+++ multicore/perf_light.h
@@ -0,0 +1,5 @@
+#ifndef PERF_LIGHT_H
+#define PERF_LIGHT_H
+#include "Ifx_Types.h"
+uint32 light_hash(uint32 seed, uint32 iterations);
+#endif
