b67f1cc91b180bc742e08fc9a3eafe5c8af551a3
Oneirarch <nazorien@gmail.com>
2026-02-25 20:22:47 +0200

    test(PERF-005s): add small header-only inline functions

--- blinky/Cpu0_Main.c
+++ blinky/Cpu0_Main.c
@@ -39,6 +39,7 @@
 #include "IfxCpu.h"
 #include "IfxScuWdt.h"
 #include "Blinky_LED.h"
+#include "perf_inline_small.h"
 
 IFX_ALIGN(4) IfxCpu_syncEvent g_cpuSyncEvent = 0;
 
@@ -64,6 +65,6 @@ void core0_main(void)
 
     while (1)
     {
-        blinkLED(); /* Make the LED blink           */
+        { volatile uint32 r = inl_small_mix(0xDEADBEEFu, 0xCAFEBABEu); (void)r; } blinkLED(); /* Make the LED blink           */
     }
 }
